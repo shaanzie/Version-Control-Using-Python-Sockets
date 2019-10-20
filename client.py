a3e62e0b1411890cc6079cb4f8aa6a1dc67c4a0f
shaanzie <ishaanlagwankar@gmail.com>
2019-10-20 18:08:18 +0530

    Updated server and client

--- client.py
+++ client.py
@@ -1,11 +1,24 @@
-import socket			 
+import socket                   # Import socket module
 
-s = socket.socket()		 
+s = socket.socket()             # Create a socket object
+host = socket.gethostname()     # Get local machine name
+port = 60000                    # Reserve a port for your service.
 
-port = 12345				
+s.connect((host, port))
+s.send("Hello server!")
 
-s.connect(('127.0.0.1', port)) 
+with open('received_file', 'wb') as f:
+    print ('file opened')
+    while True:
+        print('receiving data...')
+        data = s.recv(1024)
+        print('data=%s', (data))
+        if not data:
+            break
+        # write data to a file
+        f.write(data)
 
-print(s.recv(1024) )
-
-s.close()	 
+f.close()
+print('Successfully get the file')
+s.close()
+print('connection closed')
--- server.py
+++ server.py
@@ -1,21 +1,28 @@
-import socket			 
- 
-s = socket.socket()		 
-print( "Socket successfully created")
+import socket                   # Import socket module
 
-port = 12345				
+port = 60000                    # Reserve a port for your service.
+s = socket.socket()             # Create a socket object
+host = socket.gethostname()     # Get local machine name
+s.bind((host, port))            # Bind to the port
+s.listen(5)                     # Now wait for client connection.
 
- 
-s.bind(('', port))		 
-print("socket binded to %s" %(port)) 
+print ('Server listening....')
 
-s.listen(5)	 
-print( "socket is listening")			
+while True:
+    conn, addr = s.accept()     # Establish connection with client.
+    print ('Got connection from', addr)
+    data = conn.recv(1024)
+    print('Server received', repr(data))
 
-while True: 
-    c, addr = s.accept()	 
-    print ('Got connection from', addr) 
+    filename='1.txt'
+    f = open(filename,'rb')
+    l = f.read(1024)
+    while (l):
+       conn.send(l)
+       print('Sent ',repr(l))
+       l = f.read(1024)
+    f.close()
 
-    c.send('Thank you for connecting') 
-
-    c.close() 
+    print('Done sending')
+    conn.send('Thank you for connecting')
+    conn.close()
