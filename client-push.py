fda3cce15732ada87021911156c453561637fd14
shaanzie <ishaanlagwankar@gmail.com>
2019-11-12 09:30:38 +0530

    Push service doen

---
+++ client-push.py
@@ -0,0 +1,82 @@
+# import socket                   # Import socket module
+
+# s = socket.socket()             # Create a socket object
+# host = socket.gethostname()     # Get local machine name
+# port = 6002                    # Reserve a port for your service.
+
+# file = './README.md'
+# s.connect((host, port))
+# f = open(file, 'r')
+# contents = f.read()
+# s.send(contents)
+# f.close()
+
+# with open('received_file', 'wb') as f:
+#     print ('file opened')
+#     while True:
+#         print('receiving data...')
+#         data = s.recv(1024)
+#         print('data=', (data))
+#         if not data:
+#             break
+#         # write data to a file
+#         f.write(data)
+
+# f.close()
+# s.close()
+
+import socket                   # Import socket module
+import os
+
+filelist = str()
+             # Create a socket object
+host = socket.gethostname()     # Get local machine name
+port = 6008                    # Reserve a port for your service.
+
+
+
+
+
+for root,d_names,f_names in os.walk('./cl'):
+        for i in f_names:
+            # i = 'README.md'
+            g = open("./cl/"+i)
+            print(i)
+            contents = str()
+            contents = i + "|" + g.read()
+            print(contents)
+            s = socket.socket()
+            s.connect((host, port))
+            s.send(contents)
+            with open('./cl/received_file', 'wb') as f:
+                # print ('file opened')
+                while True:
+                    print('receiving data...')
+                    data = s.recv(10000)
+                    if not data:
+                        break
+                    # write data to a file
+                    f.write(data)
+            s.close()
+            g.close()
+            f.close()
+
+# s = socket.socket()
+# s.connect((host, port))
+# s.send("Done")
+
+f=open("./cl/received_file", "r")
+
+if f.mode == 'r':
+    contents = f.read()
+
+if(len(contents)!=0):
+    print(contents)
+else:
+    print("No changes, safe to commit")
+
+f.close()
+
+print('Successfully get the file')
+s.close()
+print('connection closed')
---
+++ server/server-push.py
@@ -0,0 +1,40 @@
+import socket                   # Import socket module
+import os
+port = 6008                # Reserve a port for your service.
+s = socket.socket()             # Create a socket object
+host = socket.gethostname()     # Get local machine name
+s.bind((host, port))            # Bind to the port
+s.listen(5)                     # Now wait for client connection.
+
+print ('Server listening....')
+contents = []
+count = 0
+charc = 0
+filecount = 0
+flag = True
+while flag:
+    s1 = ''
+    charc = 0
+    filecount += 1
+    conn, addr = s.accept()     # Establish connection with client.
+    print ('Got connection from', addr)
+    data = conn.recv(10000)
+    contents = data.split('|')
+    print("reading file ", contents[0])
+    lines1 = contents[1]
+    localfile = open(contents[0], 'w')
+    localfile.write(lines1)
+    with open("out.txt", "a") as text_file:
+        text_file.write("File successfully pushed\n")
+    ftoread = open('out.txt', 'r')
+    l = ftoread.read(1024)
+    while (l):
+       conn.send(l)
+       print('Sent ',l)
+       break
+    ftoread.close()
+
+    print('Done sending')
+    conn.send('Thank you for connecting')
+    conn.close()
+s.close()
